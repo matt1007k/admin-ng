3a81db1882c596ee884456a21bcc2b1fa4ffaeb7
matt1007k <matt1007k@gmail.com>
2018-02-27 11:39:38 -0500

    Creamos los componentes principales y configuramos las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { LoginComponent } from './login/login.component';
+import { APP_ROUTES } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NopagefoundComponent,
+    BreadcrumbsComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
